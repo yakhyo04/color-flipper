6923354944314050a047f027fe740ac6c05946fd
yakhyo04 <fenixrelaxation@gmail.com>
2021-08-30 11:17:49 +0500

    changing in app.js

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const color = document.querySelector(".color");
 
 btn.addEventListener("click", function () {
   const randomNumber = getRandomNumber();
-  // console.log(randomNumber);
 
   document.body.style.backgroundColor = colors[randomNumber];
   color.textContent = colors[randomNumber];
